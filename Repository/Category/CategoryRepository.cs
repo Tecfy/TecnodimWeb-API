b6dd775950126fef74737a13a7490a0ce756fe56
Rudolf Zanoni <rudolf@devplace.com.br>
2018-09-04 10:58:12 -0300

    [TEC] Parametrização da Categoria Pai;

--- Repository/Category/CategoryRepository.cs
+++ Repository/Category/CategoryRepository.cs
@@ -3,8 +3,10 @@ using Model.In;
 using Model.Out;
 using Model.VM;
 using SoftExpert;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Configuration;
 
 namespace Repository
 {
@@ -19,8 +21,38 @@ namespace Repository
 
             ecmCategoriesOut = SECategory.GetSECategories();
 
+            List<ECMCategoriesVM> ecmCategoriesVMs = new List<ECMCategoriesVM>();
+
+            foreach (var item in ecmCategoriesOut.result)
+            {
+                if (ValidateCategory(item, ecmCategoriesOut.result))
+                    ecmCategoriesVMs.Add(item);
+            }
+
+            ecmCategoriesOut.result = new List<ECMCategoriesVM>();
+            ecmCategoriesOut.result = ecmCategoriesVMs;
+
             registerEventRepository.SaveRegisterEvent(ecmCategoriesIn.userId.Value, ecmCategoriesIn.key.Value, "Log - End", "Repository.CategoryRepository.GetECMCategories", "");
             return ecmCategoriesOut;
-        }      
+        }
+
+        private bool ValidateCategory(ECMCategoriesVM item, List<ECMCategoriesVM> ecmCategoriesVMs)
+        {
+            bool response = false;
+
+            ECMCategoriesVM ecmCategoriesVM = new ECMCategoriesVM();
+            ecmCategoriesVM = ecmCategoriesVMs.Where(x => x.categoryId == item.parentId).FirstOrDefault();
+
+            if ((ecmCategoriesVM != null && ecmCategoriesVM.code == WebConfigurationManager.AppSettings["Repository.CategoryRepository.ParentCategory"]) || (item.code == WebConfigurationManager.AppSettings["Repository.CategoryRepository.ParentCategory"]))
+            {
+                response = true;
+            }
+            else if (ecmCategoriesVM != null && ecmCategoriesVM.parentId > 0)
+            {
+                response = ValidateCategory(ecmCategoriesVM, ecmCategoriesVMs);
+            }
+
+            return response;
+        }
     }
 }
